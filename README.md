55a229179b12790be8d90ffe1552a7a3f05029fb
David Heisnam <davidheisnam@gmail.com>
2019-03-20 23:50:26 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,83 @@
+## News.py
+
+**news.py** is a command line program that answers some useful queries related to the news database. It is useful for getting information on top articles, top authors, and to find the days on which a large number of requests to the news website led to an error.
+
+It uses the `psycopg2` library to run SQL queries against the news database.
+
+## Dependencies
+
+**psycopg2**:  
+`$ pip3 install psycopg2`
+
+## Usage
+
+The program is written for Python 3, but it runs with no problems under Python 2.
+
+To use the program, cd into the directory containing the news.py file and execute it with the available query types.
+
+The basic structure of commands for running the program is shown below.
+
+`$ python3 news.py QUERY_TYPE -n NUMBER_OF_ROWS`
+
+`QUERY_TYPE` is a required argument that can be replaced with any of the following:
+
+`top-articles` : Print top articles  
+`top-authors` : Print top authors  
+`one-percent-error-days` : Print days on which >1% of requests led to an error
+
+`-n NUMBER_OF_ROWS` is an optional argument, where `NUMBER_OF_ROWS` is a numerical value. Use it to set the number of rows returned by the query.
+
+###1. Print top articles in the database**
+`$ python3 news.py top-articles -n 3`
+
+Example output:
+
+    $ python3 news.py top-articles -n 3
+    Candidate is jerk, alleges rival - 338647 views
+    Bears love berries, alleges bear - 253801 views
+    Bad things gone, say good people - 170098 views
+
+###2. Print top authors**
+
+`$ python3 news.py top-articles -n 3`
+
+Example output:
+
+    $ python3 news.py top-articles -n 3
+    Ursula La Multa - 507594 views
+    Rudolf von Treppenwitz - 423457 views
+    Anonymous Contributor - 170098 views
+
+###3. Print days on which >1% of page requests led to an error**
+
+`$ python3 news.py one-percent-error-days -n 3`
+
+Example output:
+
+    $ python3 news.py one-percent-error-days -n 3
+    July 17, 2016 - 2.262686% errors
+    July 1, 2016 - 1.001734% errors
+    July 25, 2016 - 3.953257% errors
+
+## License
+> MIT License
+
+> Copyright (c) 2019 David Heisnam
+
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+> The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ news.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+import argparse
+import warnings
+warnings.filterwarnings("ignore") # suppress psycopg2 import warning
+import psycopg2
+import calendar
+DBNAME = "news"
+
+
+def get_top_articles(num_articles):
+    limit = ''
+    if(int(num_articles) > 0):
+        limit = ' limit ' + str(num_articles)
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute('''select articles.title, num
+                from
+                    (select articles.slug as slug, count(*) as num
+                            from articles, log
+                            where concat('/article/', articles.slug) = log.path
+                            and log.status = '200 OK'
+                            group by articles.slug
+                            order by num) as toparticles,
+                    articles
+                where articles.slug = toparticles.slug
+                group by articles.title, num
+                order by num desc%s''' % (limit))
+    articles = c.fetchall()
+    db.close()
+    return articles
+
+
+def get_top_authors(num_authors):
+    limit = ''
+    if(int(num_authors) > 0):
+        limit = ' limit ' + str(num_authors)
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute('''select authors.name, sum(num) as authorsum
+                from
+                    (select articles.slug as slug, count(*) as num
+                            from articles, log
+                            where concat('/article/', articles.slug) = log.path
+                            and log.status = '200 OK'
+                            group by articles.slug
+                            order by num) as toparticles,
+                    articles, authors
+                where authors.id = articles.author
+                and articles.slug = toparticles.slug
+                group by authors.name
+                order by authorsum desc%s''' % (limit))
+    authors = c.fetchall()
+    db.close()
+    return authors
+
+
+def get_one_percent_error_days(num_days):
+    limit = ''
+    if(int(num_days) > 0):
+        limit = ' limit ' + str(num_days)
+
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+
+    c.execute('''select * from
+                    (select allreqsquery.day, (cast(errorsquery.errorsnum as float) * 100 / allreqsquery.reqsnum) as percent
+                        from
+                            (select date(time) as day, count(*) as reqsnum from log group by day) as allreqsquery,
+                            (select date(time) as day, count(*) as errorsnum from log where status != '200 OK' group by day) as errorsquery
+                        where allreqsquery.day = errorsquery.day
+                        order by errorsquery.errorsnum desc%s) as subquery
+                    where subquery.percent > 1''' % (limit))
+    errordays = c.fetchall()
+    db.close()
+    return errordays
+
+
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Get information from news database')
+parser.add_argument("querytype", choices=['top-articles', 'top-authors', 'one-percent-error-days'], help="Query to run")
+parser.add_argument('-n', '--numrows', help='number of rows to return', type=int)
+args = parser.parse_args()
+
+if(args.querytype == 'top-articles'):
+    articles_count = 0
+    if args.numrows:
+        articles_count = args.numrows
+    for article in get_top_articles(articles_count):
+        print('%s - %d views' % (article[0], article[1]))
+
+elif(args.querytype == 'top-authors'):
+    authors_count = 0
+    if args.numrows:
+        authors_count = args.numrows
+    for authors in get_top_authors(authors_count):
+        print('%s - %d views' % (authors[0], authors[1]))
+
+elif(args.querytype == 'one-percent-error-days'):
+    days_count = 0
+    if args.numrows:
+        days_count = args.numrows
+    for day in get_one_percent_error_days(days_count):
+        print('%s %s, %s - %f%% errors' % (calendar.month_name[day[0].month], day[0].day, day[0].year, day[1]))
